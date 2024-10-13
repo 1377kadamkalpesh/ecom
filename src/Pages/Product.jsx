7ccd202249f064a17e4b3445b925655b860a45b1
1377kadamkalpesh <kadamkalpesh057@gmail.com>
2024-10-13 12:45:13 +0530

    change path in product.jsx file

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,9 +2,10 @@
 import React, { useContext } from "react";
 import { ShopeContext } from "../Context/ShopeContext";
 import { useParams } from "react-router-dom";
-import Productdisplay from "../COMPONENT/Productdisplay/productdisplay";
+import Productdisplay from "../COMPONENT/Productdisplay/Productdisplay";
 import Breadcrum from "../COMPONENT/Bredcrums/Bredcrum";
 import Popular from "../COMPONENT/Popular/Popular";
+// import Productdisplay from "../COMPONENT/Productdisplay/productdisplay";
 
 const Product = () =>{
     const{New_data} = useContext (ShopeContext);
@@ -12,14 +13,12 @@ const Product = () =>{
     const product = New_data.find((e) => e.id === Number (productId))
     return(
         <>
-        <div>
-<Breadcrum product = {product}/>
-     
-     <Productdisplay product = {product}/>
-     
-     </div>
+            <div>
+                <Breadcrum product = {product}/>
+                <Productdisplay product = {product}/>
+            </div>
         </>
     )
 }
 
-export default Product ;
+export default Product;
